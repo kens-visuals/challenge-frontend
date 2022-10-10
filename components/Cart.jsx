05c7fb5d6a4f159513970cada47512736300f9f5
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2022-10-09 17:19:37 -0700

    add quantity of orders and remove duplicates

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -4,21 +4,60 @@ import { nanoid } from 'nanoid';
 import { CartContext } from '../contexts/CartContext';
 
 export default function Cart() {
-  const { cart } = useContext(CartContext);
+  const { cart, setCart } = useContext(CartContext);
 
   const cartTotal = cart
     .reduce((total, { price = 0 }) => total + price, 0)
     .toFixed(2);
 
-  const cartItemsDisplay = cart.map(({ name, price }) => (
-    <div
-      key={nanoid()}
-      className="flex w-full items-center justify-between font-work-sans"
-    >
-      <span>{name}:</span>
-      <span>{price}</span>
-    </div>
-  ));
+  const duplicateCount = cart.reduce((acc, curr) => {
+    const str = JSON.stringify(curr);
+    acc[str] = (acc[str] || 0) + 1;
+    return acc;
+  }, {});
+
+  //   const handleRemovingFromCart = (id) => {
+  //     setCart((currentCart) => {
+  //       const indexOfItemToRemove = currentCart.findIndex(
+  //         (cartItem) => cartItem.id === id
+  //       );
+
+  //       if (indexOfItemToRemove === -1) {
+  //         return currentCart;
+  //       }
+
+  //       return [
+  //         ...currentCart.slice(0, indexOfItemToRemove),
+  //         ...currentCart.slice(indexOfItemToRemove + 1),
+  //       ];
+  //     });
+  //   };
+
+  const cartItemsDisplay = Object.entries(duplicateCount).map(
+    ([obj, count]) => {
+      const { name, price } = JSON.parse(obj);
+
+      return (
+        <div
+          key={nanoid()}
+          className="flex w-full items-center justify-between font-work-sans"
+        >
+          <span className="inline-block flex-1">{name}:</span>
+
+          <span className="mr-4 inline-block">${price}</span>
+
+          <span className="inline-block w-5">{count}x</span>
+
+          {/* <button
+            type="button"
+            onClick={() => handleRemovingFromCart(nanoid())}
+          >
+            Remove
+          </button> */}
+        </div>
+      );
+    }
+  );
 
   return (
     <div className="absolute top-[70px] left-1/2 z-10 w-[95%] max-w-md -translate-x-1/2 overflow-hidden rounded-md bg-secondary text-tertiary shadow-2xl shadow-secondary/70 md:right-0 md:top-20 md:-translate-x-20 lg:top-28 lg:right-0 lg:w-96 lg:translate-x-48">
